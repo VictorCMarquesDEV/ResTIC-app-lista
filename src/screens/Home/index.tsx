ecaf3261bd36179f2132433ad2301dfc3beff022
Victor Cavalcanti <victorcmarques.dev@gmail.com>
2024-09-15 23:44:30 -0300

    add async storage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,14 +2,16 @@ import { FlatList, StyleSheet, View, Text, Alert } from 'react-native';
 import { Task } from '../../../src/components/Task';
 import { CardNumber } from '../../../src/components/CardNumber';
 import { InputAddTask } from '../../../src/components/InputAddTask';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { TaskContext } from '../../context/TaskContext';
+import { TaskProps } from '../../utils/types';
 
 
 //app-lista: Aplicativo de Lista de Tarefas
 
 export default function Home() {
 
-    const [tasks, setTasks] = useState<{ description: string; check: boolean }[]>([]);
+    const { tasks, createTask, setTasks } = useContext(TaskContext);
     const [taskText, setTaskText] = useState("");
     const [countTask, setCountTask] = useState(0);
     const [countInProgress, setCountInProgress] = useState(0);
@@ -19,26 +21,26 @@ export default function Home() {
         if (taskText == "") {
             return Alert.alert("Texto vazio. Digite algo!");
         }
-        if (tasks.some((task) => task.description === taskText)) {
+        if (tasks.some((task) => task.title === taskText)) {
             return Alert.alert("Essa tarefa já existe!");
         }
 
-        const newTask = { description: taskText, check: false };
-        setTasks([...tasks, newTask]);
+        createTask(taskText);
         setTaskText('');
         setCountInProgress(countInProgress + 1);
     }
 
-    function handleTaskChangeStatus(taskToChange: { description: string, check: boolean }) {
-        const updatedTasks = tasks.filter((task) => task !== taskToChange)
+    function handleTaskChangeStatus(taskToChange: TaskProps) {
+        const updatedTasks = tasks.filter((task) => task.title !== taskToChange.title)
         const newTask = {
-            description: taskToChange.description,
-            check: !taskToChange.check,
+            id: taskToChange.id,
+            title: taskToChange.title,
+            status: !taskToChange.status,
         }
         updatedTasks.push(newTask);
         setTasks(updatedTasks);
 
-        if (taskToChange.check) {
+        if (taskToChange.status) {
             setCountInProgress(countInProgress - 1);
             setCountCompleted(countCompleted + 1);
         } else {
@@ -47,11 +49,11 @@ export default function Home() {
         }
     }
 
-    function handleTaskDelete(taskToDelete: { description: string, check: boolean }) {
-        const updatedTasks = tasks.filter((task) => task != taskToDelete)
+    function handleTaskDelete(taskToDelete: TaskProps) {
+        const updatedTasks = tasks.filter((task) => task.title != taskToDelete.title)
         setTasks(updatedTasks);
 
-        if (taskToDelete.check) {
+        if (taskToDelete.status) {
             setCountCompleted(countCompleted - 1);
         } else {
             setCountInProgress(countInProgress - 1);
@@ -62,8 +64,8 @@ export default function Home() {
         let totalTasks = tasks.length;
         setCountTask(totalTasks);
 
-        const inProgressTasks = tasks.filter((task) => !task.check);
-        const completedTasks = tasks.filter((task) => task.check);
+        const inProgressTasks = tasks.filter((task) => !task.status);
+        const completedTasks = tasks.filter((task) => task.status);
         setCountInProgress(inProgressTasks.length);
         setCountCompleted(completedTasks.length);
 
@@ -91,8 +93,9 @@ export default function Home() {
                     renderItem={
                         ({ item }) => (
                             <Task
-                                title={item.description}
-                                status={item.check}
+                                id={item.id}
+                                title={item.title}
+                                status={item.status}
                                 onCheck={() => handleTaskChangeStatus(item)}
                                 onRemove={() => handleTaskDelete(item)}
                             />
